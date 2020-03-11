c3e3e9734d8b74ac188f1cfdda09cccfa0df513a
RickyLei <rickylei7@gmail.com>
2020-03-11 12:51:07 -0600

    done

--- src/test.js
+++ src/test.js
@@ -1,23 +1,24 @@
 import string from './css'
 
-const demo = document.querySelector('#demo')
-const demo2 = document.querySelector('#demo2')
-
 const player = {
   id: undefined,
   time: 100,
-  n: 1,
-  init: () => {
-    demo.innerText = string.substr(0, player.n)
-    demo2.innerHTML = string.substr(0, player.n)
-    player.play()
-    player.bindEvents()
+  ui: {
+    demo: document.querySelector('#demo'),
+    demo2: document.querySelector('#demo2')
   },
   events: {
     '#btnPause': 'pause',
     '#btnPlay': 'play',
     '#btnSlow': 'slow',
     '#btnFast': 'fast',
+  },
+  n: 1,
+  init: () => {
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.innerHTML = string.substr(0, player.n)
+    player.play()
+    player.bindEvents()
   },
   bindEvents: () => {
     for (let key in player.events) {
@@ -30,12 +31,12 @@ const player = {
   run: () => {
     player.n += 1
     if (player.n > string.length) {
-      window.clearInterval(id)
+      window.clearInterval(player.id)
       return
     }
-    demo.innerText = string.substr(0, player.n)
-    demo2.innerHTML = string.substr(0, player.n)
-    demo.scrollTop = demo.scrollHeight
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.ui.demo.scrollTop = player.ui.demo.scrollHeight
   },
   play: () => {
     player.id = setInterval(player.run, player.time)
@@ -53,7 +54,6 @@ const player = {
     player.time = 0
     player.play()
   }
-
 }
 
 player.init()
